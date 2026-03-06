6f8018b8d825c5ab3e84fe96d85903212b8f6abe
Muktesh kumar <mukteshkumar108@gmail.com>
2026-03-06 17:20:17 +0530

    Add CSS styling for images and update HTML structure

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const getAllImages = document.getElementsByTagName('img')
+// const getAllImages = document.getElementsByTagName('img')
 
 // document.images //exists
 
-const cssImage = document.getElementsByClassName('css-image')
+// const cssImage = document.getElementsByClassName('css-image')
 
-const cssImage2 = document.getElementById('css-image')
+// const cssImage2 = document.getElementById('css-image')
 
+// const image = document.querySelector('.css-image')
+
+// const image = document.querySelectorAll('.css-image')
+
+const images = document.querySelectorAll('.css-image')
+
+// images.forEach((img) => {
+//   img.style.border = "4px solid red"
+// })
