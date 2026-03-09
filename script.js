f401a51e26e285f02037830b6ed955c40189b1a0
Muktesh kumar <mukteshkumar108@gmail.com>
2026-03-09 23:20:35 +0530

    Refactor image selection in script.js to use all <img> elements and update image URLs

--- script.js
+++ script.js
@@ -10,8 +10,19 @@
 
 // const image = document.querySelectorAll('.css-image')
 
-const images = document.querySelectorAll('.css-image')
+// const images = document.querySelectorAll('.css-image')
 
 // images.forEach((img) => {
 //   img.style.border = "4px solid red"
 // })
+
+const images = document.querySelectorAll('img')
+
+const imagesUrl = [
+    'https://imgs.search.brave.com/OrwVsoz4QAfDn_g6Fc2wAN8p7bVkruiMYrQZ42QipG4/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTk1/MDgxOTMzMy9waG90/by9uYXR1cmUtaGVh/cnQuanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPVl6MDZDT2dn/ejEwWUhnRU5yeDUz/Z01LdzJUVE45aVY4/Y2hScE8xZFlHUHM9',
+    'https://imgs.search.brave.com/CzM2zQAvdzQM3Xckoe3NfLSU1U6MKGGa7U4X8bvftfo/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vcGljanVt/Ym8uY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy90ZXh0LW5hdHVy/ZS1tZXRhbC1sZXR0/ZXJzLW9uLWEtcm9j/ay1pbi1ncmVlbi1m/b3Jlc3QtZnJlZS1w/aG90by5qcGc_dz02/MDAmcXVhbGl0eT04/MA',
+    'https://imgs.search.brave.com/42N4dvhIRceAXAqkrwWl20fy1xTPGyH-RYn-NBiIzUg/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9wbmcu/cG5ndHJlZS5jb20v/dGh1bWJfYmFjay9m/aDI2MC9iYWNrZ3Jv/dW5kLzIwMjQwMzI3/L3BuZ3RyZWUtYWJz/dHJhY3QtZ3JlZW4t/bmF0dXJlLWJva2Vo/LWJhY2tncm91bmQt/c29mdC1iYWNrZ3Jv/dW5kLWJsdXItb2Yt/bmF0dXJhbC1mb2xp/YWdlLWltYWdlXzE1/NjQ2ODEzLmpwZw'
+] 
+
+// images[0].src = imagesUrl[0]
+
